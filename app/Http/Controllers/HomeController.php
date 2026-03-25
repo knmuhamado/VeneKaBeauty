f02be0b8e42ebf8dac7b1ce6ea9a84307a5bc97f
David Gutiérrez <dagutierrl@eafit.edu.co>
2026-03-24 22:22:59 -0500

    quick fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,46 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Services\ProductService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\QueryException;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct(private ProductService $productService) {}
-
-    public function index(Request $request): View
+    public function index(): View
     {
         $viewData = [];
-
-        $query = (string) $request->query('query', '');
-        $categoryIds = array_values(
-            array_map('intval', array_filter((array) $request->query('category_ids', [])))
-        );
-
-        $productQuery = Product::with('category');
-
-        if (! empty($query)) {
-            $productQuery->filterByName($query);
-        }
-
-        if (! empty($categoryIds)) {
-            $productQuery->filterByCategories($categoryIds);
-        }
 
         try {
             $viewData['featuredProducts'] = Product::getTopRatedProducts();
         } catch (QueryException $e) {
             $viewData['featuredProducts'] = collect();
         }
-
-        $viewData['products'] = $productQuery->get();
-        $viewData['query'] = $query;
-        $viewData['categories'] = $this->productService
-            ->prepareCategoriesWithSelection($categoryIds);
-        $viewData['selectedCategories'] = $categoryIds;
-        $viewData['hasFilters'] = ! empty($query) || ! empty($categoryIds);
 
         return view('home.index', $viewData);
     }
