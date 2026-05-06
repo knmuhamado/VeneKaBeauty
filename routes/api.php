aa75cb220bb8002d34852af5c77b984b8759f22c
mariamny <mariamnydelvalle@gmail.com>
2026-05-05 22:39:45 -0500

    Fix service

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index');
+Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
