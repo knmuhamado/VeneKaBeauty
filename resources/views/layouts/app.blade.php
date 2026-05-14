92a34ab624adf2f1c3ea43e834d155df06ce9990
mariamny <mariamnydelvalle@gmail.com>
2026-05-13 23:24:51 -0500

    Fix font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet" />
+    @vite(['resources/sass/app.scss'])
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
     <title>@yield('title', __('app.site_name'))</title>
 </head>
@@ -105,9 +105,6 @@
             </small>
         </div>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"></script>
 
     @vite(['resources/js/app.js'])
 
