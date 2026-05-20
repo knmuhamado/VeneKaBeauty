da6e7f787f0c9e1e497138fc62e1fcae7fd8d0fd
David Gutiérrez <dagutierrl@eafit.edu.co>
2026-05-19 20:54:25 -0500

    hot fix

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
 
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware(['web', 'auth:web'])->group(function () {
     Route::post('/beauty-assistant/chat', 'App\Http\Controllers\Api\AssistantApiController@chat')->name('api.beauty-assistant.chat');
 });
